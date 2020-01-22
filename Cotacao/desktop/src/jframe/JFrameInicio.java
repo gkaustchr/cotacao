4f6ee974d4848a5f807e7fdfa34aeb3b257188e2
gkaustchr <gabrielkaustchr@gmail.com>
2020-01-22 17:46:44 -0300

    Correcao de Erros

--- Cotacao/desktop/src/jframe/JFrameInicio.java
+++ Cotacao/desktop/src/jframe/JFrameInicio.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package jframe;
 import dao.AcaoDAO;
 import dao.HistoricoDao;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
-import javax.swing.JOptionPane;
 import modelo.Acao;
 import modelo.Historico;
 
